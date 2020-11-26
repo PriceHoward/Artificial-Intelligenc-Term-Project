ac6b2ddbe949a1566f27dd385ba5270446efb709
PHoward <61019159+PriceHoward@users.noreply.github.com>
2020-11-26 11:44:27 -0600

    Update

--- GPIO example.py
+++ GPIO example.py
@@ -1,12 +1,24 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
-greenLED = 18
-blueLED = 23
-redLED = 24
-GPIO.setup(greenLED, GPIO.OUT)
-GPIO.setup(blueLED, GPIO.OUT)
-GPIO.setup(redLED, GPIO.OUT)
+
+def GPIOPinSetup():
+    GPIO.setmode(GPIO.BCM)
+    greenLED = 17
+    blueLED = 27
+    redLED = 22
+    sensorS1 = 23
+    sensorS2 = 24
+    sensorOut = 25
+    GPIO.setup(greenLED, GPIO.OUT)
+    GPIO.setup(blueLED, GPIO.OUT)
+    GPIO.setup(redLED, GPIO.OUT)
+    GPIO.setup(sensorOut, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+    GPIO.setup(sensorS2, GPIO.OUT)
+    GPIO.setup(sensorS1, GPIO.OUT)
+    
+
+
+
 def colorPicker(color):
     if(color == 'red' or color == 'RED'):
         GPIO.output(redLED, GPIO.HIGH)
@@ -22,15 +34,16 @@ def colorPicker(color):
         GPIO.output(greenLED, GPIO.HIGH)
         time.sleep(5)
         GPIO.output(greenLED, GPIO.LOW)
-def writeFile(color):
-    outFile = open('projectDataFile.txt', 'a')
-    for line in color:
-        outFile.write(color)
-        outFile.write(' ')
+#def writeFile(color):
+ #   outFile = open('projectDataFile.txt', 'a')
+  #  for line in color:
+   #     outFile.write(color)
+    #    outFile.write(' ')
 def main():
+    GPIoPinSetup()
     color = input('Please input the color you would like to be displayed: ')
     colorPicker(color)
-    writeFile(color)
+   # writeFile(color)
     GPIO.cleanup()
     
 
